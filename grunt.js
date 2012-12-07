741eed4a62e8733c538ab8ab8e46311b64f1d51c
Frédéric Perrin <frederic.perrin@gmail.com>
2012-12-07 11:59:32 +0100

    Adding watch and server tasks to grunt

--- grunt.js
+++ grunt.js
@@ -2,10 +2,12 @@ module.exports = function( grunt ) {
 	grunt.loadNpmTasks('grunt-contrib-less');
 	grunt.initConfig({
 		pkg: "<json:package.json>",
+		
 		meta: {
 			version: "<%= pkg.version %>",
 			banner: "/*! Tetra UI v<%= pkg.version %> | (MIT Licence) (c) Viadeo/APVO Corp - inspired by Bootstrap (c) Twitter, Inc. (Apache 2 Licence) */"
 		},
+
 		less: {
 			development: {
 				options: {
@@ -29,13 +31,26 @@ module.exports = function( grunt ) {
 				}
 			}
 		},
+
 		concat: {
 			'dist/tetra-ui-<%=meta.version%>.min.css': ['<banner>', 'dist/tetra-ui-<%=meta.version%>.min.css'],
 			'dist/tetra-ui-apna-<%=meta.version%>.min.css': ['<banner>', 'dist/tetra-ui-apna-<%=meta.version%>.min.css'],
 			'dist/tetra-ui-doc-<%=meta.version%>.min.css': ['<banner>', 'dist/tetra-ui-doc-<%=meta.version%>.min.css']
-		}
+		},
+
+    server: {
+      port: 8080,
+      base: '..'
+    },
+
+    watch: {
+      files: ['less/**/*.less'],
+      tasks: 'less concat'
+    },
+
 	});
 
 	// Default task.
 	grunt.registerTask('default', 'less concat');
+	grunt.registerTask('watch-serve', 'server watch');
 };
