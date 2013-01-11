570e23314568d4574f2efdc9ef40880da8b3d40c
Frédéric Perrin <frederic.perrin@gmail.com>
2013-01-11 15:36:05 +0100

    Flipswitch component

---
+++ js/tetramvc/apps/comp_flipswitch/view/flipswitch.ui.js
@@ -0,0 +1,72 @@
+tetra.view.register('flipswitch', {
+	scope: 'comp_flipswitch',
+	use: ['flipswitch'],
+	
+	constr: function(me, app, _) {
+		return {
+			events: {
+				user: {
+					'click': {
+						'.flipswitch-lbl-yes': function(e, elm) {
+
+							var container = elm.parents('.flipswitch');
+							
+							app.notify('change flipswitch state', {
+								id: container.attr('id'),
+								state: false
+							});
+
+						},
+
+						'.flipswitch-lbl-no': function(e, elm) {
+
+							var container = elm.parents('.flipswitch');
+							
+							app.notify('change flipswitch state', {
+								id: container.attr('id'),
+								state: true
+							});
+
+						},
+						
+						'.flipswitch-sliders': function(e, elm) {
+
+							var container = elm.parents('.flipswitch');
+
+							app.notify('change flipswitch state', {
+								id: container.attr('id'),
+								state: ! container.hasClass('flipswitch-no')
+							});
+
+						}
+					}
+				},
+
+				controller: {
+					'set state': function(data) {
+						var container = _('#' + data.id);
+						var yes = container.find('.flipswitch-lbl-yes input');
+						var no = container.find('.flipswitch-lbl-no input');
+
+						if (data.state) {
+							container.removeClass('flipswitch-no');
+							container.addClass('flipswitch-yes');
+							yes.prop('checked', true);
+							no.prop('checked', false);
+						} else {
+							container.removeClass('flipswitch-yes');
+							container.addClass('flipswitch-no');
+							yes.prop('checked', false);
+							no.prop('checked', true);
+						}
+
+					}
+				}
+			},
+			
+			methods: {
+				init: function() {}
+			}
+		};
+	}
+});
