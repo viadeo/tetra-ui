9baa9e7917fea5970ec03f2203f8736c0741b863
Cormac Flynn <cormac.flynn@viadeoteam.com>
2013-01-11 17:06:14 +0100

    Updates

--- src/js/conf/tetra-start.js
+++ src/js/conf/tetra-start.js
@@ -2,6 +2,6 @@ tetra.extend({
     conf: {
         env: 'jQuery',
         enableBootnode: true,
-        APPS_PATH: '/tetra-ui/src/js/coremvc/apps'
+        APPS_PATH: '/tetra-ui/src/js/coremvc/comps'
     }
 }).start();
