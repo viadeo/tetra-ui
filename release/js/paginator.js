7c1e0f9c3587f31347475d9dd32799ff2fe47efe
Frédéric Perrin <frederic.perrin@gmail.com>
2013-01-22 17:24:07 +0100

    New icons and small fixes

--- release/js/paginator.js
+++ release/js/paginator.js
@@ -1,4 +1,4 @@
-/*! Tetra UI v1.0.3 | (MIT Licence) (c) Viadeo/APVO Corp - inspired by Bootstrap (c) Twitter, Inc. (Apache 2 Licence) */
+/*! Tetra UI v1.0.5 | (MIT Licence) (c) Viadeo/APVO Corp - inspired by Bootstrap (c) Twitter, Inc. (Apache 2 Licence) */
 
 tetra.view.register('paginator', {
     scope:'paginator',
