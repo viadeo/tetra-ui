4e02b359539b8d9bdd3950e00b70730da7ef05e9
Frédéric Perrin <frederic.perrin@gmail.com>
2013-02-06 14:26:42 +0100

    Tetra UI 1.0.8 with Right To Left language support

--- release/js/navtabs.js
+++ release/js/navtabs.js
@@ -1,4 +1,4 @@
-/*! Tetra UI v1.0.7 | (MIT Licence) (c) Viadeo/APVO Corp - inspired by Bootstrap (c) Twitter, Inc. (Apache 2 Licence) */
+/*! Tetra UI v1.0.8 | (MIT Licence) (c) Viadeo/APVO Corp - inspired by Bootstrap (c) Twitter, Inc. (Apache 2 Licence) */
 
 tetra.model.register('navtabs', {
     scope:"navtabs",
