452837645de7e6f66aa8955362d272624f40bc8d
Frédéric Perrin <frederic.perrin@gmail.com>
2013-04-02 16:19:48 +0200

    Navtabs with hashes - fixes #38

--- release/js/navtabs.js
+++ release/js/navtabs.js
@@ -101,56 +101,62 @@ tetra.controller.register('navtabs', {
 });
 
 tetra.view.register('navtabs', {
-    scope:'navtabs', // application name
-    use:['navtabs'], // list of required controllers
+  scope: 'navtabs', // application name
+  use: ['navtabs'], // list of required controllers
 
-    constr:function (me, app, _) {
+  constr: function(me, app, _) {
 
-        'use strict';
+    'use strict';
 
-        return {
-            events:{
-                user:{ // list of events listened on the page
-                    'click':{
-                        '.nav-tabs a[data-href]':function (e, elm) {
-                            var data = {
-                                url:elm.attr('data-href'),
-                                targetId:elm.parents('.nav-tabs').attr('data-target-id')
-                            };
+    return {
+      events: {
+        user: { // list of events listened on the page
+          'click': {
+            '.nav a[data-href]': function(e, elm) {
+              var data = {
+                url: elm.attr('data-href'),
+                targetId: elm.parents('.nav').attr('data-target-id')
+              };
 
-                            me.methods.setActiveTab(elm);
-                            app.notify('show tab', data);
-                        }
-                    }
-                },
-
-                controller:{ // list of messages sent by controllers listened in the view
-                    'start loading':function (data) {
-                        _('#' + data.targetId).addClass('loading');
-                    },
-                    'end loading':function (data) {
-                        _('#' + data.targetId).removeClass('loading');
-                    },
-                    'set content':function (data) { // function executed at the reception
-
-                        _('#' + data.targetId).children('.section').html(data.content);
-                        var elm = _('.nav-tabs[data-target-id=' + data.targetId + ']').find('a[data-href="' + data.tabRef + '"]');
-                        me.methods.setActiveTab(elm);
-                    },
-                    'show error':function (error) {
-                        VNS.ui.growl(lang['notification.modification.save.error']);
-                    }
-                }
+              me.methods.setActiveTab(elm);
+              app.notify('show tab', data);
             },
 
-            methods:{
-                init:function () {
-                    _('.nav-tabs li.active a').click();
-                },
-                setActiveTab:function (elm) {
-                    elm.parent().addClass('active').siblings().removeClass('active');
-                }
+            '.nav a[data-section]': function(e, elm) {
+                var targetId = elm.parents('.nav').attr('data-target-id');
+                _('#' + targetId).find('.section').removeClass('active');
+                _('#' + elm.attr('data-section')).addClass('active');
+                me.methods.setActiveTab(elm);
             }
-        };
-    }
+          }
+        },
+
+        controller: { // list of messages sent by controllers listened in the view
+          'start loading': function(data) {
+            _('#' + data.targetId).addClass('loading');
+          },
+          'end loading': function(data) {
+            _('#' + data.targetId).removeClass('loading');
+          },
+          'set content': function(data) { // function executed at the reception
+            _('#' + data.targetId).children('.section').html(data.content);
+            var elm = _('.nav-tabs[data-target-id=' + data.targetId + ']').find('a[data-href="' + data.tabRef + '"]');
+            me.methods.setActiveTab(elm);
+          },
+          'show error': function(error) {
+            VNS.ui.growl(lang['notification.modification.save.error']);
+          }
+        }
+      },
+
+      methods:{
+        init: function() {
+          _('.nav-tabs li.active a').click();
+        },
+        setActiveTab: function(elm) {
+          elm.parent().addClass('active').siblings().removeClass('active');
+        }
+      }
+    };
+  }
 });
