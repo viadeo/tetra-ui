226dc8864cb5f60f6fdb5e2a350cef79cb30e982
Frédéric Perrin <frederic.perrin@gmail.com>
2013-04-18 10:58:10 +0200

    min-height rc-card behavior

--- release/js/generic_autocomplete.js
+++ release/js/generic_autocomplete.js
@@ -106,11 +106,11 @@ tetra.view.register('autocomplete', {
               switch (e.which) {
 
                 case 13: // enter
-                  // avoid enter to allow form submit
-                  e.preventDefault();
                   elm = me._container.find('.autocomplete-menu li.active');
                   var prevContainerId = me._containerId;
                   if (elm.length) {
+                      // avoid enter to allow form submit
+                      e.preventDefault();
                       me.methods.suggestions.clickOnSuggestion(elm);
                   }
                   me.methods.suggestions.hide(prevContainerId);
@@ -206,6 +206,8 @@ tetra.view.register('autocomplete', {
             }
             if (!havingSuggestion) {
                 // No suggestion to show
+                me._container.removeClass('active');
+                _(me._menu).empty();
                 return;
             }
 
