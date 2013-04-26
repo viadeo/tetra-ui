71606d98f32480f8a0d87142f7b75266ebeeaa04
Cormac Flynn <cormac.flynn@viadeoteam.com>
2013-04-26 10:52:15 +0200

    Updating package.json to ensure we have the latest dependencies

---
+++ release/js/flipbox.js
@@ -0,0 +1,27 @@
+/*! Tetra UI v1.0.29 | (MIT Licence) (c) Viadeo/APVO Corp - inspired by Bootstrap (c) Twitter, Inc. (Apache 2 Licence) */
+
+tetra.view.register('flipbox', {
+	scope: 'flipbox',
+
+	constr: function(me, app, _) {
+		'use strict';
+
+		return {
+			events: {
+				user: {
+					'click': {
+						'.bx-flip .bx-flip-turn': function (e, elm) {
+							_(elm).parents('.bx-flip').first().toggleClass('bx-flip-flipped');
+						}
+					}
+				},
+
+				controller: {}
+			},
+
+			methods:{
+				init: function () {}
+			}
+		};
+	}
+});
