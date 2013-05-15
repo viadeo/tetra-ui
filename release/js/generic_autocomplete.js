b1a14abf07c7c6673bd8716dba2dad756791627a
Frédéric Perrin <frederic.perrin@gmail.com>
2013-05-15 10:48:34 +0200

    Tetra UI 1.0.32

--- release/js/generic_autocomplete.js
+++ release/js/generic_autocomplete.js
@@ -1,4 +1,4 @@
-/*! Tetra UI v1.0.31 | (MIT Licence) (c) Viadeo/APVO Corp - inspired by Bootstrap (c) Twitter, Inc. (Apache 2 Licence) */
+/*! Tetra UI v1.0.32 | (MIT Licence) (c) Viadeo/APVO Corp - inspired by Bootstrap (c) Twitter, Inc. (Apache 2 Licence) */
 
 tetra.model.register('autocomplete', {
 	scope: 'generic_autocomplete',
