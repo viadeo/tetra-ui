da357f8e735483065e8af36e85c7d4a1a29b09f0
Frédéric Perrin <frederic.perrin@gmail.com>
2013-06-04 11:25:55 +0200

    Sync Webapp to GitHub for generic_autocomplete

--- src/js/coremvc/comps/generic_autocomplete/controller/autocomplete.ctrl.js
+++ src/js/coremvc/comps/generic_autocomplete/controller/autocomplete.ctrl.js
@@ -1,54 +1,56 @@
 tetra.controller.register('autocomplete', {
-	scope: 'generic_autocomplete',
-	use: ['autocomplete'],
+    scope:'generic_autocomplete',
+    use:['autocomplete'],
 
-	constr: function(me, app, page, orm) {
+    constr:function (me, app, page, orm) {
 
-		'use strict';
+        'use strict';
 
-		return {
-			events: {
+        return {
+            events:{
 
-				model: {
-					'autocomplete': {
-						'append': function(col) {
-							app.notify('display suggestions', col[0].get('completion'));
-						}
-					}
-				},
+                model:{
+                    'autocomplete':{
+                        'append':function (col) {
+                            app.notify('display suggestions', col[0].get('completion'));
+                        }
+                    }
+                },
 
-				view: {
+                view:{
 
-					'do query': function(data) {
-						data.uriParams = {
-							url: data.url
-						};
-						me.methods.startCountDown(function() {
-							orm('autocomplete').fetch(data);
-						}, data.typingDelay);
-					},
-					'autocompleteGeneric : click on suggestion': function(elm) {
-						page.notify('autocompleteGeneric : click on suggestion', elm);
-					}
-				}
-			},
-			methods: {
-				init: function() {
-					me.defaultTypingDelay = 150; // default delay before sending server-side request
-				},
-				startCountDown: function(callback, delay) {
-					// it is considered that user types only one field at a time, so no need to consider origin
-					// cancel previous counter if it exists
-					if(me.countDown) {
-						window.clearTimeout(me.countDown);
-					}
-					// updates typing delay if not specified
-					delay = delay || me.defaultTypingDelay;
-					// set a new counter
-					me.countDown = window.setTimeout(callback, delay);
-				}
-			}
+                    'do query':function (data) {
+                        data.uriParams = {
+                            url:data.url
+                        };
+                        me.methods.startCountDown(function () {
+                            orm('autocomplete').fetch(data);
+                        }, data);
+                    },
+                    'autocompleteGeneric : click on suggestion':function (elm) {
+                        page.notify('autocompleteGeneric : click on suggestion', elm);
+                    }
 
-		};
-	}
+                }
+            },
+            methods:{
+                init:function () {
+                    me.defaultTypingDelay = 150; // default delay before sending server-side request
+                    me.countDown = null;
+                },
+                startCountDown:function (callback, data) {
+                    // it is considered that user types only one field at a time, so no need to consider origin
+                    // cancel previous counter if it exists
+                    if (me.countDown) {
+                        window.clearTimeout(me.countDown);
+                    }
+                    // updates typing delay if precised
+                    var typingDelay = (typeof data.typingDelay !== 'undefined') ? data.typingDelay : me.defaultTypingDelay;
+                    // set a new counter
+                    me.countDown = window.setTimeout(callback, typingDelay, data);
+                }
+            }
+
+        };
+    }
 });
--- src/js/coremvc/comps/generic_autocomplete/model/autocomplete.class.js
+++ src/js/coremvc/comps/generic_autocomplete/model/autocomplete.class.js
@@ -1,31 +1,31 @@
 tetra.model.register('autocomplete', {
-	scope: 'generic_autocomplete',
+    scope:'generic_autocomplete',
 
-	req: {
-		fetch: {
-			url: '{0}',
-			uriParams: ['url'],
-			parser: function(resp, col, cond) {
-				resp.id = cond.id;
-				col[cond.uriParams.url] = {
-					id: cond.id,
-					completion: resp
-				};
-				return col;
-			}
-		}
-	},
+    req:{
+        fetch:{
+            url:'{0}',
+            uriParams:['url'],
+            parser:function (resp, col, cond) {
+                resp.id = cond.id;
+                col[cond.uriParams.url] = {
+                    id:cond.id,
+                    completion:resp
+                };
+                return col;
+            }
+        }
+    },
 
-	attr: {
-		completion: {}
-	},
+    attr:{
+        completion:{}
+    },
 
-	methods: function(attr) {
-		return {
-			validate: function(attr, errors) {
-				return errors;
-			}
-		};
-	}
+    methods:function (attr) {
+        return {
+            validate:function (attr, errors) {
+                return errors;
+            }
+        };
+    }
 
 });
