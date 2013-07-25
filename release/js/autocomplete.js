ce2310a68edf71337535bdc4b282f6c6899d9e9e
Frédéric Perrin <frederic.perrin@gmail.com>
2013-07-25 15:00:17 +0200

    Removing unused .add-on items on form fields

--- release/js/autocomplete.js
+++ release/js/autocomplete.js
@@ -1,4 +1,4 @@
-/*! Tetra UI v1.0.33 | (MIT Licence) (c) Viadeo/APVO Corp - inspired by Bootstrap (c) Twitter, Inc. (Apache 2 Licence) */
+/*! Tetra UI v1.0.34 | (MIT Licence) (c) Viadeo/APVO Corp - inspired by Bootstrap (c) Twitter, Inc. (Apache 2 Licence) */
 
 tetra.controller.register('autocomplete', {
 	scope: 'autocomplete',
--- release/js/file_upload.js
+++ release/js/file_upload.js
@@ -1,4 +1,4 @@
-/*! Tetra UI v1.0.33 | (MIT Licence) (c) Viadeo/APVO Corp - inspired by Bootstrap (c) Twitter, Inc. (Apache 2 Licence) */
+/*! Tetra UI v1.0.34 | (MIT Licence) (c) Viadeo/APVO Corp - inspired by Bootstrap (c) Twitter, Inc. (Apache 2 Licence) */
 
 tetra.model.register('file_upload', {
 	scope: 'file_upload',
