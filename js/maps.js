744d6007bf7c925cf27316c283f2a12e568865ba
Frédéric Perrin <frederic.perrin@gmail.com>
2013-09-25 16:59:16 +0200

    Maps API - fixes #129

--- js/maps.js
+++ js/maps.js
@@ -593,13 +593,13 @@ tetra.controller.register('geocode', {
 	constr : function(me, app, page, orm) { return {
 		events : {
 			controller : {
-				'maps: geocode' : function(obj, cbk){
+				'maps: geocode' : function(params){
 					if(typeof(google) === "undefined") return;
 					var geocoder = new google.maps.Geocoder();
-					geocoder.geocode(obj, function(results, status) {
-						if (status !== google.maps.GeocoderStatus.OK) return cbk(null);
-						if(obj.hasOwnProperty('address')) return cbk(results[0]);
-						if(obj.hasOwnProperty('latlng')) return cbk(results[1].formatted_address);
+					geocoder.geocode(params, function(results, status) {
+						if (status !== google.maps.GeocoderStatus.OK) return params.cbk(null);
+						if(params.hasOwnProperty('address')) return params.cbk(results[0]);
+						if(params.hasOwnProperty('latlng')) return params.cbk(results[1].formatted_address);
 					});
 				}
 			}
