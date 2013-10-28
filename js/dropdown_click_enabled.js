fa1cf935b52d73f37f5292122d6d09e02625fc41
Frédéric Perrin <frederic.perrin@gmail.com>
2013-10-28 17:05:52 +0100

    Tetra UI 1.3.2

--- js/dropdown_click_enabled.js
+++ js/dropdown_click_enabled.js
@@ -1,4 +1,4 @@
-/*! Tetra UI v1.3.1 | (MIT Licence) (c) Viadeo/APVO Corp - inspired by Bootstrap (c) Twitter, Inc. (Apache 2 Licence) */
+/*! Tetra UI v1.3.2 | (MIT Licence) (c) Viadeo/APVO Corp - inspired by Bootstrap (c) Twitter, Inc. (Apache 2 Licence) */
 
 tetra.controller.register('dropdown', {
 	scope: 'dropdown',
