d561ae8e4813ca7bafe2c2df0d1de2e11e5abc2f
Duvillier Antoine <duvillierantoine@gmail.com>
2013-11-13 14:42:00 +0100

    remove useless grunt-contrib-less

--- Gruntfile.js
+++ Gruntfile.js
@@ -170,7 +170,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-contrib-clean');
