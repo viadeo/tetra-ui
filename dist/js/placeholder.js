cd6cc2588c992e9e5cb00a17526f80dda33329d1
Duvillier Antoine <duvillierantoine@gmail.com>
2014-02-20 11:45:19 +0100

    update nav-menu design

--- dist/js/placeholder.js
+++ dist/js/placeholder.js
@@ -1,4 +1,4 @@
-/*! tetra-ui v1.4.7 - 2013 | (MIT Licence) (c) Viadeo/APVO Corp - inspired by Bootstrap (c) Twitter, Inc. (Apache 2 Licence) */
+/*! tetra-ui v1.4.7 - 2014 | (MIT Licence) (c) Viadeo/APVO Corp - inspired by Bootstrap (c) Twitter, Inc. (Apache 2 Licence) */
 
 tetra.view.register("placeholder", {
     scope: "placeholder",
