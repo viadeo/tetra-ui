b9fdfa681b675bc777a90d0ca2b3971ba3a627cb
antoine <duvillierantoine@gmail.com>
2014-05-09 14:57:07 +0200

    remove js packages

--- tasks/options/concat.js
+++ tasks/options/concat.js
@@ -3,29 +3,8 @@ var files = require('../build/profile');
 module.exports = {
   js: {
     files: [{
-      '<%= path.js.dist %>/conf/tetra-js.js': 'src/js/conf/tetra-js.js',
-      '<%= path.js.dist %>/autocomplete.js': '<%= path.js.src %>/autocomplete/**/*.js',
-      '<%= path.js.dist %>/dropdown.js': ['<%= path.js.src %>/dropdown/**/*.js','!<%= path.js.src %>/dropdown/view/click_enabled.ui.js'],
-      '<%= path.js.dist %>/dropdown_click_enabled.js': '<%= path.js.src %>/dropdown/view/click_enabled.ui.js',
-      '<%= path.js.dist %>/flipswitch.js': '<%= path.js.src %>/flipswitch/**/*.js',
-      '<%= path.js.dist %>/generic_autocomplete.js': '<%= path.js.src %>/generic_autocomplete/**/*.js',
-      '<%= path.js.dist %>/growl.js': '<%= path.js.src %>/growl/**/*.js',
-      '<%= path.js.dist %>/navtabs.js': '<%= path.js.src %>/navtabs/**/*.js',
-      '<%= path.js.dist %>/paginator.js': '<%= path.js.src %>/paginator/**/*.js',
-      '<%= path.js.dist %>/popin.js': '<%= path.js.src %>/popin/**/*.js',
-      '<%= path.js.dist %>/file_upload.js': '<%= path.js.src %>/file_upload/**/*.js',
-      '<%= path.js.dist %>/highlight.js': '<%= path.js.src %>/highlight/**/*.js',
-      '<%= path.js.dist %>/maps.js': '<%= path.js.src %>/maps/**/*.js',
-      '<%= path.js.dist %>/toggle.js': '<%= path.js.src %>/toggle/**/*.js',
-      '<%= path.js.dist %>/yesno.js': '<%= path.js.src %>/yesno/**/*.js',
-      '<%= path.js.dist %>/placeholder.js': '<%= path.js.src %>/placeholder/**/*.js',
-      '<%= path.js.dist %>/iePlaceholder.js': '<%= path.js.src %>/ie/**/*.js',
-      '<%= path.js.dist %>/alert.js': '<%= path.js.src %>/alert/**/*.js',
-      '<%= path.js.dist %>/flipbox.js': '<%= path.js.src %>/flipbox/**/*.js',
-      '<%= path.js.dist %>/select.js': '<%= path.js.src %>/select/**/*.js'
-    },
-    files
-    ]
+      '<%= path.js.dist %>/conf/tetra-js.js': 'src/js/conf/tetra-js.js'
+    }]
   },
   less: {
     options: {
