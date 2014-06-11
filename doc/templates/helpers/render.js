9d1871f7c0eb632b781e83c01b4b1d7c58caac63
Duvillier Antoine <duvillierantoine@gmail.com>
2014-06-11 15:01:13 +0200

    finalize new doc

--- doc/templates/helpers/render.js
+++ doc/templates/helpers/render.js
@@ -2,8 +2,7 @@ module.exports.register = function (Handlebars, options)  {
   Handlebars.registerHelper('render', function(name, ctx, hash) {
     var ps = Handlebars.partials;
     if(typeof(ps[name]) === 'undefined') {
-      // temp dev; then throw an error
-      console.log('partial \'' + name + '\' does not exists');
+      //console.log('partial \'' + name + '\' does not exists');
       return '';
     }
     if(typeof ps[name] !== 'function') {
